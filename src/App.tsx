b20f0902b349a4ebeb30487d1643c73710a1556d
Anastasiia Holubko <nastya12092006@gmail.com>
2026-06-02 15:32:45 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useState } from 'react';
+import { getTodos } from './api';
+import { useAppDispatch } from './app/hooks';
+import { setTodos } from './features/todos';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    setIsLoading(true);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+    getTodos()
+      .then(todosFromServer => {
+        dispatch(setTodos(todosFromServer));
+      })
+
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {isLoading && <Loader />}
+              <TodoList />
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      <TodoModal />
+    </>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { clearCurrentTodo } from '../../features/currentTodo';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
 export const TodoModal: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!currentTodo) {
+      return;
+    }
+
+    setUser(null);
+    setIsLoading(true);
+
+    getUser(currentTodo.userId)
+      .then(userFromServer => {
+        setUser(userFromServer);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [currentTodo]);
+
+  const handleCloseModal = () => {
+    dispatch(clearCurrentTodo());
+  };
+
+  if (!currentTodo) {
+    return null;
+  }
+
   return (
     <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
+      <div className="modal-background" onClick={handleCloseModal} />
 
-      <Loader />
+      {isLoading && <Loader />}
 
       <div className="modal-card">
         <header className="modal-card-head">
@@ -14,27 +49,36 @@ export const TodoModal: React.FC = () => {
             className="modal-card-title has-text-weight-medium"
             data-cy="modal-header"
           >
-            Todo #3
+            Todo #{currentTodo.id}
           </div>
 
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
+          <button
+            type="button"
+            className="delete"
+            data-cy="modal-close"
+            onClick={handleCloseModal}
+          />
         </header>
 
         <div className="modal-card-body">
           <p className="block" data-cy="modal-title">
-            fugiat veniam minus
+            {currentTodo.title}
           </p>
 
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
+          {user && (
+            <p className="block" data-cy="modal-user">
+              {currentTodo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
+              {' by '}
+
+              <a href={`mailto:${user.email}`}>{user.name}</a>
+            </p>
+          )}
         </div>
       </div>
     </div>
